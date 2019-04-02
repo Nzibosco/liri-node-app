db250077b7e9f567bdb73bfcf2ccdc519d5822c8
Bosco <bosco@MacBook-Air.local>
2019-04-01 20:08:03 -0500

    finishing up and readme

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# liri-node-app
+# Liri
+
+This is liri. Liri is a node application and a command line interface (CLI) app that allows you to search movies, concerts and songs. 
+
+Liri takes in user input and returns information related to the users search criteria. 
+
+The applications relies on NPM packages and axios dependencies. To use this application, you need to have package.JSON installed in the repository where you cloned this application. 
+
+### how to use Liri
+
+  ##### searching for songs 
+
+We use Spotify API to get important data on a song being searched. 
+Enter the following in your terminal to search songs: 
+
+```node liri-node-app spotify-this-song <The song you want to search>``` 
+
+The application will return suggested songs, associated artists, albums, and preview urls where they are available. 
+
+   ##### searching for movies 
+
+We use OMDB API to search data on movies. 
+Enter the follow code in your terminal to search movies: 
+```node liri-node-app movie-this <the name of movie you want>``` 
+
+The application returns important information on the movie such as title, actors, plot, and ratings. 
+
+
+   ##### searching for concerts 
+
+To search for concerts, we use Bands in Town API to search concerts about an artist or whatever input the user is searching for. 
+To get the exact concert time, we use ```moment.js``` library to output readable time format. 
+
+Enter the code below to search concerts: 
+```node liri-node-app concert-this <artists or bands>``` 
+
+You get possible concerts from the artists / band, venue and time. 
+
+
+------------------------------------------------------------
+
+Aside, there is another cool search where random data search on spotify. This data are copied from ```random.txt``` file to search on spotify. 
+
+enter the following code to perform such search: ```node liri-node-app do-what-it-says``` 
+
+The gif attached will explain further functionalities of the application. 
+
+![GitHub Logo](liri-node-app/liri.gif)
+Format: ![Alt Text](url)
--- liri.js
+++ liri.js
@@ -1,57 +1,128 @@
-require("dotenv").config(); 
-var axios = require("axios"); 
+require("dotenv").config();
+var axios = require("axios");
 
-var moment = require("moment"); 
+var moment = require("moment");
 
 
 var lineBreak = ("\n-------------------------------------------\n");
 
 // searching songs on spotify 
 
-// 1. grab user input 
-var userInput = process.argv[2];
+// 1. grab user search 
+var search = process.argv[2];
+var input = process.argv.slice(3).join(" ");
+
+switch (search) {
+  case "spotify-this-song":
+    songData();
+    break;
+
+  case "movie-this":
+    movieData();
+    break;
+
+  case "concert-this":
+    bandDetails();
+    break;
+
+  case "do-what-it-says":
+    whatItSay();
+    break;
+};
+
+
 var keys = require("./keys.js"); // linking api and secret keys stored in keys.js file
 
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-spotify.search({ type: "track", query: userInput, limit: "4" }, 
-function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-  console.log( lineBreak+ "Artists: " + data.tracks.items[0].artists[0].name + 
-  "\n\n" + "Song Name: " + data.tracks.items[0].name + 
-  "\n\n" + "Album: " + data.tracks.items[0].album.name +
-  "\n\n" + "Preview link: " + data.tracks.items[0].preview_url +"\n\n" + lineBreak);
-  });
+// searching songs info 
+
+function songData() {
+var keys = require("./keys.js");
+
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+  spotify.search({ type: "track", query: input },
+    function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      var music = data.tracks.items;
+      for (var i = 0; i < music.length; i++) {
+        console.log(lineBreak + "Artists: " + music[i].artists[0].name +
+          "\n\n" + "Song Name: " + music[i].name +
+          "\n\n" + "Album: " + music[i].album.name +
+          "\n\n" + "Preview link: " + music[i].preview_url + "\n\n" + lineBreak);
+      };
+
+    });
+};
 
 // searching movies on OMDB
+function movieData() {
+  var queryURL = "https://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy"
+  axios.get(queryURL).then(function (response) {
+    var info = response.data;
+    console.log("\n\n");
+    console.log("Title: " + info.Title);
+    console.log("Release Year: " + info.Year);
+    console.log("IMDB Rating: " + info.Ratings[0].Value);
+    console.log("Rotten Tomatoes Rating: " + info.Ratings[1].Value);
+    console.log("Country: " + info.Country);
+    console.log("Language: " + info.Language);
+    console.log("Plot: " + info.Plot);
+    console.log("Actors: " + info.Actors + lineBreak);
 
+  });
+};
 
-axios.get("http://www.omdbapi.com/?apikey=67ad12&t=avengers").then(function(response){ 
-    console.log(lineBreak + "* Title: "+ response.data.Title + "\n\n"+
-    "* Year: " + response.data.Year + "\n\n" +
-    "* IMDB Rating: " + response.data.imdbRating + "\n\n" +
-    "* Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n\n" +
-    "* Production Country: " + response.data.Country + "\n\n" + 
-    "* Languages: " + response.data.Language + "\n\n" + 
-    "* Plot: " + response.data.Plot + "\n\n" + 
-    "* Actors: " + response.data.Actors + lineBreak
-    );
-});
+// search concert 
 
-axios.get("https://rest.bandsintown.com/artists/khalid/events?app_id=codingbootcamp")
-.then(function (response){
-    var results = response.data;
-    //console.log(results);
-    for (var i =0; i<results.length; i++){
-        //for (var i = 0; i<results; i++){
-    var time =results[i].datetime;
-    time = moment(time).format("MM/DD/YYYY - HH:mm");
-    console.log(lineBreak + "Venue: " + results[i].venue.name + "\n\n" +
-    "City: " + results[i].venue.city + "\n\n" + 
-    "Date: " + time + lineBreak);
-    };    
-});
+function bandDetails() {
+  axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+    .then(function (response) {
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var time = results[i].datetime;
+        //format the event time
+        time = moment(time).format("MM/DD/YYYY - HH:mm");
+        console.log(lineBreak + "Venue: " + results[i].venue.name + "\n\n" +
+          "City: " + results[i].venue.city + "\n\n" +
+          "Date: " + time + lineBreak);
+      };
+    });
+};
+
+// do what it says 
+
+function whatItSay() {
+  var fs = require("fs");
+  //Read random.txt file
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error)
+    }
+    //Split data into array
+    var textArr = data.split(",");
+    userCommand = textArr[0];
+    userInput = textArr[1];
+
+    spotify.search({ type: "track", query: userInput },
+      function (err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        var music = data.tracks.items;
+        for (var i = 0; i < music.length; i++) {
+          console.log(lineBreak + "Artists: " + music[i].artists[0].name +
+            "\n\n" + "Song Name: " + music[i].name +
+            "\n\n" + "Album: " + music[i].album.name +
+            "\n\n" + "Preview link: " + music[i].preview_url + "\n\n" + lineBreak);
+        };
+
+      });
+  })
+};
+
 
